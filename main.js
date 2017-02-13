ad1c9795b9e0e5664b93f8bcf631362ed282bb79
ashleyrtatum <ashleyhuston@Ashleys-MacBook-Pro.local>
2017-02-13 18:52:10 -0500

    homework_day_4_foundations

---
+++ main.js
@@ -0,0 +1,85 @@
+//Part 1
+
+var givenName
+//undetermined
+
+var givenName = "Brett";
+console.log(givenName);
+
+var givenName = "givenName";
+console.log(givenName);
+
+
+//Part 2
+
+var givenName = "Brett";
+
+var Greeting = "Hello how are you";
+
+console.log(Greeting + " " + givenName + "?")
+
+
+//Part 3
+
+var high = 50;
+var low = 10;
+
+var answer = 50 - 10;
+console.log(answer);
+
+var answer = 50 - 5;
+console.log(answer);
+
+var math = high - low;
+console.log(math);
+
+math = high - "5";
+console.log(math);
+
+
+//Part 4
+
+var name = "Ashley";
+
+var born = 1990;
+var today = 2017;
+
+var bday = today - born;
+console.log(name + " is" + " " + bday + " years old");
+
+//Part 4.b
+
+var yourName = "Ashley";
+var instructorName = "Joe";
+
+
+console.log(yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName);
+
+
+
+//Part 5
+
+var myAge = 45;
+var isOld;
+
+if (myAge > 100) {
+  isOld = true;
+  console.log(isOld);
+} else {
+  isOld = false;
+  console.log(isOld);
+}
+
+
+//Part 6
+var firstName = "Ashley";
+var lastName = "Huston";
+var fullname = firstName + " " + lastName;
+
+console.log(fullname);
+
+
+var yearBorn = 1990;
+var currentYear = 2017;
+var age = currentYear - yearBorn;
+console.log(age);
